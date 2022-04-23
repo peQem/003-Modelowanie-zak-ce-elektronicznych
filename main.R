215fffc0470c8a9c0ef3d494045896473d2e9702
peQem <40581567+peQem@users.noreply.github.com>
2022-04-23 02:27:42 +0200

    plots

--- main.R
+++ main.R
@@ -6,6 +6,7 @@
 library(tidyverse)
 library(forecast)
 library(fitdistrplus)
+install.packages()
 
 
 
@@ -34,22 +35,46 @@ plotdist(z_term$I, breaks = 60, histo = TRUE, demp = TRUE)
 
 descdist(z_term$I, boot = 1000)
 
-fw <- fitdist(z_term$I*1000, "norm")
+fw1 <- fitdist(z_term$I*1000, "norm")
 
 plot(fw)
 
+gofstat(fw1)
 
 
 # wifi --------------------------------------------------------------------
 
 
+## as normal
 plot(z_wifi$I)
 plotdist(z_wifi$I, breaks = 60, histo = TRUE, demp = TRUE)
 
 descdist(z_wifi$I, boot = 1000)
 
-fw <- fitdist(z_wifi$I*1000, "norm")
+fw2 <- fitdist(z_wifi$I*1000, "norm")
 
-plot(fw)
+plot(fw2)
 
-summary(fw)
+summary(fw2)
+
+
+
+# wifi half ---------------------------------------------------------------
+
+z_wifi_half <- z_wifi$I[z_wifi$I>=mean(z_wifi$I)]
+z_wifi_half <- z_wifi_half + (-min(z_wifi_half))
+z_wifi_half <- z_wifi_half[z_wifi_half>0]
+
+descdist(z_wifi_half, boot = 1000)
+
+# as half of Laplace ->
+fw3 <- fitdist(z_wifi_half, "exp")
+
+plot(fw3)
+
+
+# as beta
+# as half of Laplace ->
+fw3 <- fitdist(z_wifi_half, "beta")
+
+plot(fw3)
